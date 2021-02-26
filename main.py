cded2165d6f7c84b7c48a09177311840bf6b9591
Carter4502 <carterbelisle@yahoo.com>
2021-02-25 19:11:16 -0600

    test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def displayQuestion(questionText, answers, correctAnswerNumber):
     # questionText: text that will be displayed
     # answers: array of possible answers to the question
     # correctAnswerNumber: index of the correct answer (1-indexed)
-    # returns 1 for correct and 0 for incorrect
+    # returns 1 for correct guess and 0 for incorrect guess
 
     print(questionText)
     for x in range (0, len(answers)):
@@ -17,5 +17,6 @@ def displayQuestion(questionText, answers, correctAnswerNumber):
 
 # example usage
 # displayQuestion("what is the first letter of the alphabet?", ["a", "b", "c", "d"], 1)
+
 
 
